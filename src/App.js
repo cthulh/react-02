067885a71d9c0ea41932ca9d3aef4eef7c7e2b81
cthulh <tryglavson@hotmail.co.uk!>
2018-01-17 10:29:24 +0000

    state in App added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Bob', age: 32 },
+      { name: 'Tim', age: 34 },
+      { name: 'Claire', age: 36 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
-        <Person name='Bob' age='32'/>
-        <Person name='Tim' age='34'>I love running!</Person>
-        <Person name='Claire' age='36'/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I love running!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // the above code is compiled to the below commands:
